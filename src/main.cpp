39c4215a2b3d9de1b308f9195a6dad2cd83e0ee5
dadanhdyt <dadanhidyt@gmail.com>
2024-12-08 18:31:44 +0700

    ;;;

--- src/main.cpp
+++ src/main.cpp
@@ -1,51 +1,57 @@
 #include <Arduino.h>
+#include <ArduinoJson.h>
 #include <SPI.h>
 #include <MFRC522.h>
 #include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
 int wifiStatus;
 const char *ssid = "Ruzman";
+const char *api_url = "http://192.168.43.50/loker";
 const char *password = "12345678";
-
+#define LOKER_ID 1;
 #define RST_PIN D1
 #define SDA_PIN D2
 #define RELAY_LOCKDOOR D3
-#define BUZER_JOKOWI D8
+#define BUZZER D8
 MFRC522 mfrc522(SDA_PIN, RST_PIN);
 
-void soundCardValid()
+void soundCardValid(int soundInterval = 100)
 {
-  digitalWrite(BUZER_JOKOWI, HIGH);
-  delay(100);
-  digitalWrite(BUZER_JOKOWI, LOW);
-  delay(100);
-  digitalWrite(BUZER_JOKOWI, HIGH);
-  delay(100);
-  digitalWrite(BUZER_JOKOWI, LOW);
+  digitalWrite(BUZZER, HIGH);
+  delay(soundInterval);
+  digitalWrite(BUZZER, LOW);
+  delay(soundInterval);
+  digitalWrite(BUZZER, HIGH);
+  delay(soundInterval);
+  digitalWrite(BUZZER, LOW);
 }
 
 void setup()
 {
   Serial.begin(9600);
   // set pin mode
-  pinMode(BUZER_JOKOWI, OUTPUT);
+  pinMode(BUZZER, OUTPUT);
   pinMode(RELAY_LOCKDOOR, OUTPUT);
   digitalWrite(RELAY_LOCKDOOR, HIGH); // Relay off by default
-  digitalWrite(BUZER_JOKOWI,LOW);
+  digitalWrite(BUZZER, LOW);
   // start spi
   SPI.begin();
   mfrc522.PCD_Init();
   // connect to wifi
   WiFi.begin(ssid, password);
-  //koneksi kan ke jaringan
+  // koneksi kan ke jaringan
   while (WiFi.status() != WL_CONNECTED)
   {
     delay(500);
     Serial.print(".");
+    soundCardValid();
   }
-  //cek wifi koneksi nya apakah sudah konek apa belum
-  if ( WiFi.status() == WL_CONNECTED ) {
+  // cek wifi koneksi nya apakah sudah konek apa belum
+  if (WiFi.status() == WL_CONNECTED)
+  {
     Serial.println("Koneksi Dengan Wifi: ");
     Serial.print(WiFi.localIP());
+    soundCardValid(300);
   }
 }
 // rad data
@@ -68,32 +74,51 @@ void loop()
   wifiStatus = WiFi.status();
   if (wifiStatus == WL_CONNECTED)
   {
+    HTTPClient http;
+    WiFiClient wf_client;
 
     if (mfrc522.PICC_IsNewCardPresent() && mfrc522.PICC_ReadCardSerial())
     {
       String cardUID = readData();
       Serial.print("Detected UID: ");
       Serial.println(cardUID);
-
-      if (cardUID == " E3 6A 46 FE" || cardUID == " 05 8F 3B 69 B3 A2 00")
+      http.begin(wf_client, String(api_url) + String("/save_card.php"));
+      http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+      // http.begin(wf_client, String(api_url) + String("/save_card.php"));
+      int httpResponKode = http.POST("rfid=" + cardUID + "&loker=1");
+      if (httpResponKode > 0)
       {
-        soundCardValid();
-        Serial.println("STMIKKK");
-        digitalWrite(RELAY_LOCKDOOR, LOW);  // Aktifkan relay (coba HIGH jika ini tidak aktif)
-        delay(2000);                        // Relay aktif selama 2 detik
-        digitalWrite(RELAY_LOCKDOOR, HIGH); // Matikan relay
-        Serial.println("Relay Deactivated");
+        Serial.print("HTTP Response code: ");
+        Serial.println(httpResponKode);
+        String payload = http.getString();
+        if (payload == cardUID || cardUID == " 05 8F 3B 69 B3 A2 00")
+        {
+          soundCardValid();
+          digitalWrite(RELAY_LOCKDOOR, LOW);  // Aktifkan relay (coba HIGH jika ini tidak aktif)
+          delay(2000);                        // Relay aktif selama 2 detik
+          digitalWrite(RELAY_LOCKDOOR, HIGH); // Matikan relay
+        }
+        else
+        {
+          digitalWrite(BUZZER, HIGH);
+          delay(1000);
+          digitalWrite(BUZZER, LOW);
+          digitalWrite(RELAY_LOCKDOOR, HIGH); // Pastikan relay mati jika kartu tidak cocok
+        }
       }
       else
       {
-        digitalWrite(BUZER_JOKOWI, HIGH);
-        delay(1000);
-        digitalWrite(BUZER_JOKOWI, LOW);
-        Serial.println("Access Denied");
-        digitalWrite(RELAY_LOCKDOOR, HIGH); // Pastikan relay mati jika kartu tidak cocok
+        Serial.print("Error code: ");
+        Serial.println(httpResponKode);
+        String payload = http.getString();
+        Serial.println(payload);
       }
+      http.end();
+      
     }
-  } else {
+  }
+  else
+  {
     Serial.print("Wifi tidak konek bos");
   }
 }
